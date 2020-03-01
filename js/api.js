b3d1b914d48dcc2f079135ad0f332e259a526634
Blake Collins <blakeac369@gmail.com>
2020-03-01 00:54:15 -0800

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -1,84 +1,91 @@
-/*jshint esversion: 6 */
-const employeeAPI = "https://randomuser.me/api/?results=12&nat=us";
-const employeeList = document.getElementsByClassName("employee");
-const overlay = document.getElementById("overlay");
+const employeeAPI = 'https://randomuser.me/api/?results=12&inc=name,location,email,dob,cell,picture&nat=US';
+const modalContainer = document.querySelector('.modalContainer');
+const mainContainer = document.querySelector('.container');
+const gridContainer = document.querySelector('.employeeGrid');
+const modal = document.querySelector('#modal');
+let employees = [];
 
-var employeePhone;
-var employeeAddress;
-var employeeDOB;
+//fetching employees
+fetch(employeeAPI)
+    .then(response => response.json())
+    .then(data => data.results)
+    .then(data => {
+        employees.push(data);
+        employees = employees[0];
+        generateEmployees(employees);
+    })
+    .catch(err => console.log('Something went wrong...Beep Boop'))
 
-//Requesting Information
-function fetchData(url, HTML){
-    fetch(url)
-        .then(response => response.json())
-        .then(data => generateHTML(HTML, data.results[0]))
-        .catch(error => console.log("Looks like there was a problem, beep boop...error"));
-}
 
-//generating the HTML needed to display employees
-function generateHTML(){
-    const employeeImage = data.picture.large;
-    const employeeFullName = data.name.first + data.name.last;
-    const employeeEmail = data.email;
-    const employeeCity = data.location.city;
-    employeePhone = data.phone;
-    employeeAddress = data.location.street.number + data.location.name + "," + data.location.state + data.postcode;
 
-    let year = data.dob.date.substring(2,4);
-    let month = data.dob.date.substring(5,7);
-    let day = data.dob.date.substring(8,10);
-    let dob = month + "/" + day + "/" + year;
-    employeeDOB = "Birthday: " + dob;
+// Creating employee cards
+function generateEmployees(data) {
+    const employees = data;
+    console.log(employees.length);
+    var statusHTML = '';
+    employees.forEach((employee, index) => {
+        let picture = employee.picture.large;
+        let firstName = employee.name.first;
+        let lastName = employee.name.last;
+        let email = employee.email;
+        let city = employee.location.city;
 
-    employee.innerHTML = `
-        <img class="image" src=${employeeImage}>
-            <div class="details">
-                <h4  class="fullName">${employeeFullName}</h4>
-                <p class="email">${employeeEmail}></p>
-                <p class="city">${employeeCity}</p>
+        statusHTML += `
+        
+            <div class="card" data-index="${index}">
+                <img class ="profilePicture" src="${picture}">
+                    <h3 class="employee_name">${firstName} ${lastName}</h3>
+                    <p>${email}</p>
+                    <p>${city}</p>
             </div>
-    
-    `;
-}
-
-function displayEmployees(){
-    for( let i =0; i<employeeList.lenght; i++){
-        fetchData(employeeAPI, employeeList[i]);
-    }
-}
-
-window.addEventListener("load", function(){
-    displayEmployees();
-});
-
-
-//overlay display
-for (let i = 0; i< employeeList.lenght; i+=1){
-    employeeList[i].addEventListener("click", e =>{
-        overlay.style.display = "block";
-        let id = e.target.id;
-        if(id == null || id ==""){
-            id = e.target.parentNode.id;
-            if(id == null || id ==""){
-                id=e.target.parentNode.parentNode.id; }
-     }
-let parent = document.getElementById(id);
-     overlay.innerHTML = `
-        <p class="backward"><</p>
-        <div class="overlayContent" id="overlayContent">
-        <p class="close">X</p>
-        ${parent.innerHTML}
-        <div class="contant">
-        <p>${employeePhone}</p>
-        <p>${employeeAddress}</p>
-        <p>${employeeDOB}</p>
-        </div>
-        </div>
-        <p class="forward">></p>
-     `;
-     document.querySelector(".close").addEventListener("click", () => {
-        overlay.style.display = "none";
-
+        `;
     });
-});
+    gridContainer.innerHTML = statusHTML;
+
+    gridContainer.querySelectorAll('.card').forEach((card, index) => {
+        card.addEventListener('click', (e) => { 
+            generateModal(employees[index]);
+        }); 
+    })
+
+
 }
+
+//modal window
+function generateModal(employee) {
+
+    function modalHtml(employee) {
+        let dob = new Date(Date.parse(employee.dob.date)).toLocaleDateString(navigator.language);
+        modalContainer.innerHTML = `
+            <div class="modal-box">
+                <span class="close">&times;</span>
+                <div class="modal-info">
+                    <div class="modal-content">
+                        <image src="${employee.picture.large}" class="modal-picture">
+                        <h2 class="employee_name">${employee.name.first} ${employee.name.last}</h2>
+                        <p>${employee.email}</p>
+                        <p>${employee.cell}</p>
+                        <p>${employee.location.street.number} 
+                            ${employee.location.street.name}<br>
+                            ${employee.location.city},<br>
+                            ${employee.location.state} 
+                            ${employee.location.postcode}</p>
+                        <p>Birthday: ${dob}</p>
+                    </div>
+                </div>
+            </div>
+        `;  
+    }
+
+    modalContainer.style.display = 'block';
+    modalHtml(employee);
+}
+
+
+//Close modal window
+modalContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('close') || e.target.classList.contains('modal-container')) {
+        document.querySelector('.modal-box').innerHTML = "";
+        modal.style.display = 'none';
+    }
+})
